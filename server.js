609f489079014fe58b41a1876fc110416242efd8
alexjeffcott@gmail.com <alexjeffcott@gmail.com>
2020-10-02 12:11:52 +0200

    updated and audited dependencies

--- server.js
+++ server.js
@@ -11,29 +11,29 @@ const PORT = process.env.PORT || 3000
 const app = express()
 
 if (isDevelopment) {
-    const compiler = webpack(config)
-    app.use(
-        require('webpack-dev-middleware')(compiler, {
-            publicPath: config.output.publicPath,
-        })
-    )
-    app.use(
-        require(`webpack-hot-middleware`)(compiler, {
-            log: false,
-            path: `/__webpack_hmr`,
-            heartbeat: 10 * 1000,
-        })
-    )
-} else {
-    const DIST_DIR = `${__dirname}/dist`
-    const HTML_FILE = path.join(DIST_DIR, '/index.html')
-    app.use(express.static(DIST_DIR))
-
-    app.get('*', (req, res) => {
-        res.sendFile(HTML_FILE)
+  const compiler = webpack(config)
+  app.use(
+    require('webpack-dev-middleware')(compiler, {
+      publicPath: config.output.publicPath,
     })
+  )
+  app.use(
+    require(`webpack-hot-middleware`)(compiler, {
+      log: false,
+      path: `/__webpack_hmr`,
+      heartbeat: 10 * 1000,
+    })
+  )
+} else {
+  const DIST_DIR = `${__dirname}/dist`
+  const HTML_FILE = path.join(DIST_DIR, '/index.html')
+  app.use(express.static(DIST_DIR))
+
+  app.get('*', (req, res) => {
+    res.sendFile(HTML_FILE)
+  })
 }
 
 app.listen(PORT, () => {
-    console.log(`App listening to ${PORT} with env: ${config.mode}....`)
+  console.log(`App listening to ${PORT} with env: ${config.mode}....`)
 })
